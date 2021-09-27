ab30932242f319e5563abaefba4def81cabfd892
Matthew Duffy <mattduff707@hotmail.com>
2021-09-27 12:59:32 -0500

    Added cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const cors = require('cors');
+
 const snippetsRouter = require('./routes/snippets.routes');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
