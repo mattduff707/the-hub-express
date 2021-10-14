faa4c6d88ea6722ed512a67a6a57cdcf473c5c40
Matthew Duffy <mattduff707@hotmail.com>
2021-10-14 18:31:27 -0500

    Trying allowAccessDomain middleware

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors());
 require('dotenv').config({ path: './config.env' });
 const dbo = require('./db/conn');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(allowCrossDomain);
 const snippetsRouter = require('./routes/snippets.routes');
 const tasksRouter = require('./routes/tasks.routes');
 
