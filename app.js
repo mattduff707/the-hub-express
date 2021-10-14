2dcfb850a4a955c1ec7b7805c6442956a8c08aa1
Matthew Duffy <mattduff707@hotmail.com>
2021-10-14 18:38:47 -0500

    Added allowAccessDomain middleware

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const dbo = require('./db/conn');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
 
 app.use(allowCrossDomain);
 const snippetsRouter = require('./routes/snippets.routes');
