25c050a74d0111028a9dcc9f8b090e7672ce52f5
Matthew Duffy <matthewduffy@pop-os.localdomain>
2021-10-19 19:10:48 -0500

    Added donelist init route

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config({ path: './config.env' });
-const dbo = require('./db/conn');
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const dbo = require("./db/conn");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 };
 
 app.use(allowCrossDomain);
 
-const snippetsRouter = require('./routes/snippets.routes');
-const tasksRouter = require('./routes/tasks.routes');
+const snippetsRouter = require("./routes/snippets.routes");
+const tasksRouter = require("./routes/tasks.routes");
 
-app.use('/', snippetsRouter);
-app.use('/', tasksRouter);
-app.get('/', (req, res) => {
-  res.json({ message: 'hello world' });
+app.use("/", snippetsRouter);
+app.use("/", tasksRouter);
+app.get("/", (req, res) => {
+  res.json({ message: "hello world" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   dbo.connectToServer(function (err) {
-    if (err) console.error(err, 'heyo');
+    if (err) console.error(err, "heyo");
   });
   console.log(`Server started on port ${PORT}`);
 });
